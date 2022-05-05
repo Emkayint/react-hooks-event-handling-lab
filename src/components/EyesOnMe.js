a086a68a1517fe8eeb7383ff4aa3eafcc931fa9f
Martin <mkmartiones@gmail.com>
2022-05-05 14:16:59 +0300

    ft: eyes on me section

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,7 +1,14 @@
 // Code EyesOnMe Component Here
 
 function EyesOnMe(){
+  return(
+    <button 
+      onFocus = { () => console.log('Good!')}
+      onBlur = {() => console.log("Hey! Eyes on me!")}
+    >
 
+    </button>
+  )
 }
 
 export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,7 +1,9 @@
 // Code Keypad Component Here
 
 function Keypad(){
-
+  return (
+    <input onChange={ () => console.log(`Entering password...`)} style = {{ width : "200px", height : "30px"}} placeholder = "Password"/>
+  )
 }
 
 export default Keypad;
